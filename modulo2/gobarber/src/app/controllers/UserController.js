606ab80af324a1a30478678b50039d717fd41a29
Alex de Melo Silva <alexmsilva@gmail.com>
2019-08-29 00:41:23 -0300

    Adds input validation with yup

--- modulo2/gobarber/src/app/controllers/UserController.js
+++ modulo2/gobarber/src/app/controllers/UserController.js
@@ -1,7 +1,19 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
     async create(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6)
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
         const userExists = await User.findOne({ where: { email: req.body.email } });
 
         if (userExists) {
@@ -19,6 +31,23 @@ class UserController {
     }
 
     async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            old_password: Yup.string().min(6),
+            password: Yup.string()
+                .min(6)
+                .when('old_password', (oldPasswod, field) => oldPasswod ? field.required() : field),
+            confirm_password: Yup.string()
+                .when('password', (password, field) => 
+                    password ? field.required().oneOf([Yup.ref('password')]) : field
+                )
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
         const { email } = req.body;
 
         const user = await User.findByPk(req.userId);
