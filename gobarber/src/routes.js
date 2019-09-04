edcd677765be161f0b0cc03626b84a52b8716669
Alex de Melo Silva <alexmsilva@gmail.com>
2019-09-04 20:06:49 -0300

    Fixes migration and creates table files

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -18,8 +19,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/avatar', upload.single('avatar'), (req, res) => {
-    res.json({ message: 'ok' });
-})
+routes.post('/files', upload.single('file'), FileController.create);
 
 export default routes;
