68ef64b2d2fdd0e8a061f54870fa4e3d3f543e4c
Alex de Melo Silva <alexmsilva@gmail.com>
2019-09-04 23:58:20 -0300

    Adds date-fns to handle appointments date checks

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -15,10 +16,28 @@ class AppointmentController {
 
         const { provider_id, date } = req.body;
 
+        /**
+         * Checks id provider_id belongs to provider
+         */
         const isProvider = await User.findOne({ where: { id: provider_id, provider: true } });
-
         if (!isProvider) {
             return res.status(401).json({ error: 'You can only create appointments with providers' });
+        }
+
+        /**
+         * Checks for past dates
+         */
+        const hourStart = startOfHour(parseISO(date));
+        if (isBefore(hourStart, new Date())) {
+            return res.status(401).json({ error: 'Past dates are not permitted' });
+        }
+
+        /**
+         * Checks date availability
+         */
+        const checkAvailability = await Appointment.findOne({ where: { provider_id, canceled_at: null, date } });
+        if (checkAvailability) {
+            return res.status(400).json({ error: 'Appointment date is not available' });
         }
 
         const appointment = await Appointment.create({
