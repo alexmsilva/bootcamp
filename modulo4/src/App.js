40c32d8417c97747005726005094245d5fbb838c
Alex de Melo Silva <alexmsilva@gmail.com>
2019-10-13 10:49:55 -0300

    Starts first react app

---
+++ modulo4/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+    return <h1>Hello Módulo 4</h1>
+}
+
+export default App;
--- modulo4/webpack.config.js
+++ modulo4/webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'public')
+        contentBase: path.resolve(__dirname, 'public'),
+        host: '0.0.0.0'
     },
     module: {
         rules: [
