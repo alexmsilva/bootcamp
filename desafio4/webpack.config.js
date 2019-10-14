f6caf4fc0735fb8912d3e3facb510f9b4a6cb733
Alex de Melo Silva <alexmsilva@gmail.com>
2019-10-14 20:39:39 -0300

    Creates a minimum react app

---
+++ desafio4/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        host: '0.0.0.0',
+        port: 8080
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
