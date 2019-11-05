614872411258c59854ccb29db4eb4290c10f4b27
Alex de Melo Silva <alexmsilva@gmail.com>
2019-11-04 23:59:30 -0300

    Gets user data from github

--- modulo6/src/pages/Main/index.js
+++ modulo6/src/pages/Main/index.js
@@ -1,8 +1,53 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Main() {
-    return <View />;
+import { Container, Form, Input, SubmitButton } from './styles';
+import api from '../../services/api';
+
+export default class Main extends Component {
+    state =  {
+        newUser: '',
+        users: []
+    };
+
+    handleAddUser = async () => {
+        const { users, newUser } = this.state;
+
+        const response = await api.get(`/users/${newUser}`);
+        const data = {
+            name: response.data.name,
+            login: response.data.login,
+            bio: response.data.bio,
+            avatar: response.data.avatar_url
+        };
+
+        this.setState({
+            users: [...users, data],
+            newUser: ''
+        });
+
+        Keyboard.dismiss();
+    };
+
+    render() {
+        return <Container>
+            <Form>
+                <Input
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    placeholder="Adicionar Usuário"
+                    value={this.state.newUser}
+                    onChangeText={text => this.setState({ newUser: text })}
+                    returnKeyType="send"
+                    onSubmitEditing={this.handleAddUser}
+                />
+                <SubmitButton onPress={this.handleAddUser}>
+                    <Icon name="add" size={20} color="#fff" />
+                </SubmitButton>
+            </Form>
+        </Container>;
+    }
 }
 
 Main.navigationOptions = {
